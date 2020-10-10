e0444f1c40704b845cd8b78199f7cd6c612dadde
swaibu <rumbiihas@gmail.com>
2020-10-10 06:41:44 +0300

    add vamoo icons

--- Gulpfile.js
+++ Gulpfile.js
@@ -144,9 +144,9 @@ function vendors() {
 // watch tasks
 function watchfiles() {
   gulp.watch('./src/assets/scss/**/*', gulp.series(htmlReset, styles, browserSyncReload));
-  gulp.watch('src/pages/**/*', html);
+  gulp.watch('src/pages/**/**/*', html);
   gulp.watch('src/assets/images/**/*', images);
-  gulp.watch('src/{layouts,includes,helpers,partials,data}/**/*', gulp.series(htmlReset, html, browserSyncReload));
+  gulp.watch('src/{layouts,includes,helpers,partials,data}/**/**/*', gulp.series(htmlReset, html, browserSyncReload));
 }
 const build = gulp.series(clean, gulp.parallel(html, styles, vendors, modules, images, icons));
 exports.default = gulp.series(build, gulp.parallel(browserSync, watchfiles));
